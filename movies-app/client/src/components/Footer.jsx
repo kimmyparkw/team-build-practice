d4593cc6731c4bd45b79a08cf2d5239b86ed9ced
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-08-27 11:23:45 -0500

    removed react boilerplate and replaced with app's components

---
+++ movies-app/client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Site created by Pineapples</h3>
+        </footer>
+    )
+}
+
+
+export default Footer
