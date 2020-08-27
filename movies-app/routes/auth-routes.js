7d7649de75469016cff62e2f3b064c2ff919720c
Shannon Maloy <shannon.maloy@gmail.com>
2020-08-27 14:08:54 -0400

    Completed logging in and logging out routes.  Passport Complete

--- movies-app/routes/auth-routes.js
+++ movies-app/routes/auth-routes.js
@@ -7,3 +7,41 @@ const usersController = require('../controllers/users-controller')
 //new user data being passed to usersController to create to new user
 authRouter.post('/register', usersController.create)
 
+//route for user to login
+authRouter.post('/login', passport.authenticate('local', {
+    successRedirect: '/api/auth/verify',
+    failureRedirect: '/api/auth/verify',
+    failureFlash: true,
+}))
+
+//Verify user information
+authRouter.get('/verify', (req, res) => {
+    if (req.user) return res.status(200).json({
+        message: 'ok',
+        auth: true,
+        data: {
+            user: req.user,
+        }
+    })
+    else return res.status(400).json({
+        message: 'Login Failed',
+        auth: false,
+        data: {
+            user: null
+        }
+    })
+})
+
+//Logout route
+authRouter.get('/logout', (req, res) => {
+    req.logout()
+    res.json({
+        message: 'Logged Out',
+        auth: false,
+        data: {
+            user: null
+        }
+    })
+})
+
+module.exports = authRouter;
